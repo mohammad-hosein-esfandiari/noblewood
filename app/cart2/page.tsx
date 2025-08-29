258fc1eac43a8fea277430897f6ec8da2f3bb99d
Mohammad <mohammadhosseinesfandiari1@gmail.com>
2025-08-29 19:22:16 +0330

    Working on user cart

--- app/cart2/page.tsx
+++ app/cart2/page.tsx
@@ -1,9 +1,10 @@
+import ShoppingCart from '@/components/pages/ShoppingCart/ShoppingCart'
 import React from 'react'
 
-export default function ShoppingCart() {
+export default function page() {
   return (
-    <div className='pt-[80px] max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 '>
-
+    <div className='pt-[95px] py-8 min-h-screen max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 '>
+      <ShoppingCart/>
     </div>
   )
 }
